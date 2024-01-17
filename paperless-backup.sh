3f1585e2f108f9ec7c9efb3685b328347b715c2c
Chris Langhans <lcn2abt@de.bosch.com>
2024-01-17 17:02:37 +0100

    feat: add logging to backup.sh

--- paperless-backup.sh
+++ paperless-backup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "sourcing paperless-config.sh"
 source "paperless-config.sh"
 
+echo "exporting documents"
 pushd "${PAPERLESS_CONFIG_PATH}" || exit
 docker compose exec -T webserver document_exporter ../export
 
@@ -11,11 +13,13 @@ NAS_PATH="$HOME/fritznas"
 STORAGE_PATH="${NAS_PATH}/ASMT-2115-01/paperless"
 BACKUP_LOG_FILE="${STORAGE_PATH}/backup-${DATE}.log"
 
+echo "mounting NAS"
 mkdir -p "${NAS_PATH}"
 mount "${NAS_PATH}" || exit 1
 
-# rsync backup to NAS
-rsync -avz --delete "${PAPERLESS_EXPORT_PATH}" "${STORAGE_PATH}" > "${BACKUP_LOG_FILE}"
+echo "copying documents to NAS"
+rsync -avz --progress --delete "${PAPERLESS_EXPORT_PATH}" "${STORAGE_PATH}" | tee "${BACKUP_LOG_FILE}"
 
+echo "unmounting NAS"
 umount "${NAS_PATH}"
 popd || exit
